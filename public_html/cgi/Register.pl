a7510dc75c28e10e50045ba5e2be203fd8781d18
Watchandmeet <watchandmeet@gmail.com>
2014-11-28 08:43:44 -0500

    handling registration success

--- public_html/cgi/Register.pl
+++ public_html/cgi/Register.pl
@@ -9,16 +9,23 @@ my $password = param('password');
 
 # Check if username already exists
 sub exist(){
-        return 1;
+    return 0;
 }
 
 # Render error page
-sub displayError(){
-        print "Username is taken. <a href='../registration.html'>Try again</a> or go to <a href='../index.html'>homepage</a>.\n";
+sub error(){
+    print "Username is taken. <a href='../registration.html'>Try again</a> or go to <a href='../index.html'>homepage</a>.\n";
+}
+
+# Render success page
+sub success(){
+    print "Account succesfully created, <a href='../login.html'>sign in</a> now!\n";
 }
 
 # Display resulting HTML page
 print "Content-type: text/html\n\n";
 if (exist()){
-        displayError();
+    error();
+} else {
+    success();
 }
