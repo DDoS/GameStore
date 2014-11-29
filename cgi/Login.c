89be1013fcbcf5adbc2c7ba46b15da2a1a1d185d
Aleksi Sapon <aleksi.sapon-cousineau@mail.mcgill.ca>
2014-11-28 19:40:04 -0500

    Add Member.csv test values, and start reading the DB

--- cgi/Login.c
+++ cgi/Login.c
@@ -2,6 +2,49 @@
 #include <stdlib.h>
 #include <string.h>
 
+char* readLine(FILE* file) {
+    int current = ftell(file);
+    int n = 0;
+    int c;
+    do {
+        c = fgetc(file);
+        n++;
+    } while (c != '\n' && c != '\r' && c != EOF);
+    n--;
+
+    if (c == EOF) {
+        return NULL;
+    }
+
+    char* line = (char*) calloc(n + 1, sizeof(char));
+
+    fseek(file, current, SEEK_SET);
+    int i;
+    for (i = 0; i < n; i++) {
+        line[i] = fgetc(file);
+    }
+
+    fgetc(file);
+    line[i] = '\0';
+
+    return line;
+}
+
+void getExistingUserPassword(char* name, char* pass, int n) {
+    FILE* members = fopen("../database/Members.csv", "r");
+
+    char* line;
+    while (1) {
+        line = readLine(members);
+        if (!line) {
+            break;
+        }
+        free(line);
+    }
+
+    fclose(members);
+}
+
 void getUserInfo(char* input, char* name, char* pass, int n) {
     char* token;
 
@@ -15,8 +58,8 @@ void getUserInfo(char* input, char* name, char* pass, int n) {
 }
 
 int main() {
-    int n = atoi(getenv("CONTENT_LENGTH")) + 1;
-    char* content = (char*) calloc(n, sizeof(char));
+    int n = atoi(getenv("CONTENT_LENGTH"));
+    char* content = (char*) calloc(n + 1, sizeof(char));
 
     int i = 0;
     for (i = 0; i < n; i++) {
@@ -35,9 +78,14 @@ int main() {
 
     getUserInfo(content, username, password, 201);
 
-    printf("username: %s, password: %s", username, password);
+    printf("username: %s, password: %s\n", username, password);
+
+    getExistingUserPassword(NULL, NULL, 0);
 
     free(content);
+
+    printf("Done!\n");
+    fflush(stdout);
 
     return EXIT_SUCCESS;
 }
